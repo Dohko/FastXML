cf0d45381d1dd49d70b11c53b31fe4254d041e9e
Morgan FITUSSI <mfitussi@gmail.com>
2018-05-08 12:34:48 +0200

    Updated unit test for chained tags support

---
+++ Source/Tag.swift
@@ -0,0 +1,13 @@
+//
+//  Tag.swift
+//  FastXML
+//
+//  Created by Morgan Fitussi on 07/05/2018.
+//  Copyright © 2018 SwiftXML. All rights reserved.
+//
+
+import UIKit
+
+class Tag: NSObject {
+
+}
